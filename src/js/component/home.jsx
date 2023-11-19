926b3b0239f24c4f2a3910d18ef0ee49ccdb5f2f
Haisa Reyes <130813009+HaisaeReyes@users.noreply.github.com>
2023-11-19 21:07:02 +0000

    commit 1

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,33 @@
+import { useEffect, useState } from "react";
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import { SimpleCounter } from "./simpleCounter";
 
 //create your first component
 const Home = () => {
+	const [counter, setCounter] = useState(0)
+
+	useEffect(() =>{
+		const interval = setInterval(() => {
+			setCounter(counter => counter + 1)
+		}, 1000)
+		return () => clearInterval(interval)
+	}, [counter])
+
+	function calculateSeconds(aCounter, placeValue){
+		return Math.floor(aCounter/placeValue) % 10
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+
+		<SimpleCounter 
+			sixDigit = {calculateSeconds(counter, 100000)}
+			fiveDigit = {calculateSeconds(counter, 10000)}
+			fourDigit = {calculateSeconds(counter, 1000)}
+			threeDigit = {calculateSeconds(counter, 100)}
+			twoDigit = {calculateSeconds(counter, 10)}
+			oneDigit = {calculateSeconds(counter, 1)}
+		/>
+
 	);
 };
 
